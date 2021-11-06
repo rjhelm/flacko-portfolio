3eba7764766ecfe8cca0ddef864cc8eb04301a9c
rjhelm <rjhelm@github.io>
2021-11-05 19:41:59 -0600

    structure for hero hero section added

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -2,16 +2,29 @@ import React from "react";
 import Image from 'react-bootstrap/Image';
 import { Container } from "react-bootstrap/Container";
 import hero from "../../assets/images/hero.png"
+import { Col, Row } from "react-bootstrap";
 
 const Hero = () => {
     return (
-        <Container className="hero" >
-            <div className="md=6 hero-text">
-                
-            </div>
-            <div className="md=4 hero-image">
-                <Image src={hero} fluid />
-            </div>
+        <Container className="hero" id="home">
+            <Row>
+                <Col className="md=6 hero-text">
+                    <h1 className="hero-header">
+                    <strong className="highlight"></strong>
+                    </h1>
+                    <h2 className="hero-subheader">
+                    <strong className="highlight"></strong>
+                    </h2>
+                    <div>
+                        <p className="hero-content">
+                            
+                        </p>
+                    </div>
+                </Col>
+                <Col className="md=4 hero-image">
+                    <Image src={hero} fluid />
+                </Col>
+            </Row>
         </Container>
     );
 
