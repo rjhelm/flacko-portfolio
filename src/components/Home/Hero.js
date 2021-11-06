2970fcb17cd10bf70f5e5738c7830a6ec80cd0b5
rjhelm <rjhelm@github.io>
2021-11-05 19:51:27 -0600

    added structure for the homepage

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,28 +1,19 @@
 import React from "react";
-import Image from 'react-bootstrap/Image';
 import { Container } from "react-bootstrap/Container";
-import hero from "../../assets/images/hero.png"
 import { Col, Row } from "react-bootstrap";
 
 const Hero = () => {
     return (
-        <Container className="hero" id="home">
+        <Container className="hero-section">
             <Row>
-                <Col className="md=6 hero-text">
-                    <h1 className="hero-header">
-                    <strong className="highlight"></strong>
-                    </h1>
-                    <h2 className="hero-subheader">
-                    <strong className="highlight"></strong>
-                    </h2>
-                    <div>
-                        <p className="hero-content">
-                            
-                        </p>
-                    </div>
+                <Col className="md=4 hero-content">
+                    
                 </Col>
-                <Col className="md=4 hero-image">
-                    <Image src={hero} fluid />
+                <Col className="md=4 hero-content">
+                    
+                </Col>
+                <Col className="md=4 hero-conttent">
+
                 </Col>
             </Row>
         </Container>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Image } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
+import hero from '../../assets/images/hero.jpg'
+import Hero from './Hero'
+
+const Home = () => {
+    return (
+        <section>
+            <Container fluid className="home-section" id="home">
+                <Container className="home-section">
+                    <Row>
+                        <Col className="md=6 home-text">
+                            <h1 className="home-header">
+                                <strong className="highlight"></strong>
+                            </h1>
+                            <h2 className="home-subheader">
+                                <strong className="highlight"></strong>
+                            </h2>
+                            <div>
+                                <p className="home-content">
+
+                                </p>
+                            </div>
+                        </Col>
+                        <Col className="md=4 home-image">
+                            <Image src={hero} fluid />
+                        </Col>
+                    </Row>
+                </Container>
+                <Hero />
+            </Container>
+        </section>
+    )
+}
+
+export default Home
