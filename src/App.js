01ff9cc8ccc2bccefe2347dd1f649da839c2ceab
rjhelm <rjhelm@github.io>
2021-11-10 17:47:51 -0700

    added particle animation effect

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
 import "./style.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
