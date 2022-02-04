6251589f999e9fb7d550be1fe441f6c12ca4e554
rjhelm <rjhelm@github.io>
2022-02-03 19:24:57 -0700

    new

--- src/components/molecules/index.js
+++
@@ -1,2 +0,0 @@
-export { default as SectionHeader } from './SectionHeader';
-export { default as CountUpNumber } from './CountUpNumber';
--- src/views/Home/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Customization } from './Customization';
-export { default as Hero } from './Hero';
-export { default as Hub } from './Hub';
-export { default as Partners } from './Partners';
-export { default as Pricings } from './Pricings';
