2d42d42c177eb09aecd28fde8e8023f0002e9204
rjhelm <rjhelm@github.io>
2022-02-05 12:29:43 -0700

    added a svg file with reusable svg icons

---
+++ flacko/src/assets/SubComponents/SocialIcons.js
@@ -0,0 +1,7 @@
+import { motion } from 'framer-motion'
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+import { LinkedIn, Github, Twitter, Youtube } from '../Components/AllSvgs'
+import { DarkTheme } from '../../Components/Themes'
+
