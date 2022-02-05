95a67ef2e9b485c4f8a596c3e5e960974c1299f2
rjhelm <rjhelm@github.io>
2022-02-04 20:51:43 -0700

    added about me section:

---
+++ flacko/src/Components/About.js
@@ -0,0 +1,77 @@
+import React from "react";
+import styled, { keyframes, ThemeProvider } from "styled-components";
+import { DarkTheme } from "./Themes";
+import LogoComponent from  "../SubComponents/LogoComponent";
+import SocialIcons from "../SubComponents/SocialIcons";
+import PowerButton from "../SubComponents/PowerButton";
+import ParticleComponent from "../SubComponents/ParticleComponent";
+import BigTitle from "../SubComponents/BigTitle";
+
+const Box = styled.div`
+    background-color: ${props => props.theme.body};
+    width: 100vw;
+    height: 100vh;
+    position: relative;
+    overflow: hidden;
+`
+
+const float = keyframes`
+    0% { transform: translateY(0px) }
+    50% { transform: translateY(15px) translateX(15px) }
+    100% { transform: translateY(-10px) }
+`
+
+const Spaceman = styled.div`
+    position: absolute;
+    top: 10%;
+    right: 5%;
+    width: 20vw;
+    animation: ${float} 4s ease infinite;
+    img {
+        width: 100%;
+        height: auto;
+    }
+`
+const Main = styled.div`
+    border: 2px solid ${(props) => props.theme.text};
+    color${(props) => props.theme.text};
+    padding: 2rem;
+    width: 50vw;
+    height: 60vh;
+    z-index: 3;
+    line-height: 1.5;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: calc(0.6rem + 1vw);
+    backdrop-filter: blur(4px);
+    position: absolute;
+    left: calc(5rem + 5vw);
+    top: 10rem;
+    font-family: 'Ubuntu Mono', monospace;
+    font-style: italic;
+`
+
+const AboutPage = () => {
+    <ThemeProvider theme={DarkTheme}>
+        <Box>
+            <LogoComponent />
+            <SocialIcons />
+            <PowerButton />
+            <ParticleComponent />
+
+            <Spaceman>
+                <img src={" "} alt="spaceman"/>
+            </Spaceman>
+            <Main>
+                I'm Ryan a Full Stack Web Developer from Utah in the U.S. I have a background in MERN Stack development <br />
+                but have also studied C#, Python, Java, and C++. I have a passion for learning new technologies and I'm always <br />
+                looking to broaden my skillset and knowledge. 
+            </Main>
+
+            <BigTitle text="ABOUT" top="10%" left="5%" />
+        </Box>
+    </ThemeProvider>
+}
+
+export default AboutPage;
---
+++ flacko/src/Components/Themes.js
@@ -0,0 +1,15 @@
+export const lightTheme = {
+    body: "#A017E8",
+    text: "#FFFFFF",
+    fontFamily: "'Source Sans Pro', sans-serif",
+    bodyRgba: "252, 246, 244",
+    textRgba: "0, 0, 0",
+}
+
+export const DarkTheme = {
+    body: "#000000",
+    text: "#3817E8",
+    fontFamily: "'Source Sans Pro', sans-serif",
+    bodyRgba: "252, 246, 244",
+    textRgba: "0, 0, 0",
+}
